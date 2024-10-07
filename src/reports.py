e6f445c5421e05dbb717795d6c4de53f5ee4bf7b
Anton Fomenko <antonfomenko1234@gmail.com>
2024-10-07 16:01:22 +0300

    add tests for spending_by_category

--- src/reports.py
+++ src/reports.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 from functools import wraps
+from src.utils import get_operations_data
 
 import pandas as pd
 
@@ -42,16 +43,18 @@ def spending_by_category(df: pd.DataFrame, name_category: str, date: str = None)
     stop_year = stop_date.year
     stop_day = stop_date.day
     start_month = stop_month - 3
+    start_year = stop_year
     if start_month <= 0:
         start_month = 12 - abs(start_month)
-    start_date = datetime.datetime(year=stop_year, month=start_month, day=stop_day)
+        start_year = stop_year - 1
+    start_date = datetime.datetime(year=start_year, month=start_month, day=stop_day)
 
     sorted_df = df.loc[df["Статус"] == "OK"]
     sorted_df["Дата операции"] = pd.to_datetime(sorted_df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
     sorted_df = sorted_df[(sorted_df["Дата операции"] >= start_date) & (sorted_df["Дата операции"] <= stop_date)]
 
-    # pattern = fr"{name_category}".lower()
-
     sorted_df = sorted_df.loc[sorted_df["Категория"] == name_category]
+
+    sorted_df["Дата операции"] = sorted_df["Дата операции"].astype(str)
 
     return sorted_df
